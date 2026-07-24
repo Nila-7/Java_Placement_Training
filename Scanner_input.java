ee4bf6d3abbf157ced9cb4dbb00f592a0e823fb6
Nithiyanila <182837985+Nila-7@users.noreply.github.com>
2026-07-25 05:24:36 +0530

    Java input sing Scanner is added

---
+++ Scanner_input.java
@@ -0,0 +1,11 @@
+package java_p;
+
+import java.util.Scanner; 
+public class Scanner_input {
+	public static void main(String args[]) {
+	      Scanner sc = new Scanner(System.in);
+	      System.out.println("Enter the number:");
+          int num = sc.nextInt();
+          System.out.println("The number is:"+num);
+	}
+}
